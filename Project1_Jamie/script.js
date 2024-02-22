825331d21965db35dedb7e8a11ec2f959e68f920
jamiejessical <144182973+jamiejessical@users.noreply.github.com>
2024-02-21 22:01:10 -0500

    Add files via upload

---
+++ Project1_Jamie/script.js
@@ -0,0 +1,195 @@
+myFunction()
+
+function myFunction() {
+    var x = document.getElementById("book1Open");
+    var y = document.getElementById("book2Open");
+    var z = document.getElementById("book3Open");
+    var q = document.getElementById("book4Open");
+    var a = document.getElementById("book0Open");
+    var b = document.getElementById("book0");
+    var c = document.getElementById("book");
+    var d = document.getElementById("book3");
+    var e = document.getElementById("book4");
+    var f = document.getElementById("book2");
+    var screenWidth = window.innerWidth;
+    if (x.style.width === "0px") {
+      if (screenWidth <= 900) {
+          x.style.width = "63vw";
+          b.style.display = "none";
+          c.style.display = "none";
+          d.style.display = "inline-flex";
+          e.style.display = "inline-flex";
+          f.style.display = "inline-flex";
+      } else {
+          x.style.width = "40vw";
+      }
+      y.style.width = "0px";
+      z.style.width = "0px";
+      q.style.width = "0px";
+      a.style.width = "0px";
+    } else {
+      x.style.width = "0px";
+      y.style.width = "0px";
+      z.style.width = "0px";
+      q.style.width = "0px";
+      a.style.width = "0px";
+      b.style.display = "inline-flex";
+      c.style.display = "inline-flex";
+      d.style.display = "inline-flex";
+      e.style.display = "inline-flex";
+      f.style.display = "inline-flex";
+  }
+}
+
+
+
+function myFunction2() {
+    var x = document.getElementById("book1Open");
+    var y = document.getElementById("book2Open");
+    var z = document.getElementById("book3Open");
+    var q = document.getElementById("book4Open");
+    var a = document.getElementById("book0Open");
+    var b = document.getElementById("book0");
+    var c = document.getElementById("book");
+    var d = document.getElementById("book3");
+    var e = document.getElementById("book4");
+    var f = document.getElementById("book2");
+    var screenWidth = window.innerWidth;
+    if (y.style.width === "0px") {
+      if (screenWidth <= 900) {
+        y.style.width = "63vw";
+        b.style.display = "none";
+        c.style.display = "none";
+        d.style.display = "inline-flex";
+        e.style.display = "inline-flex";
+    } else {
+        y.style.width = "40vw";
+    }
+        x.style.width = "0px";
+        z.style.width = "0px";
+        q.style.width = "0px";
+        a.style.width = "0px";
+      } else {
+        x.style.width = "0px";
+        y.style.width = "0px";
+        z.style.width = "0px";
+        q.style.width = "0px";
+        a.style.width = "0px";
+        b.style.display = "inline-flex";
+        c.style.display = "inline-flex";
+        d.style.display = "inline-flex";
+        e.style.display = "inline-flex";
+        f.style.display = "inline-flex";
+      }
+}
+
+function myFunction3() {
+    var x = document.getElementById("book1Open");
+    var y = document.getElementById("book2Open");
+    var z = document.getElementById("book3Open");
+    var q = document.getElementById("book4Open");
+    var a = document.getElementById("book0Open");
+    var b = document.getElementById("book0");
+    var c = document.getElementById("book");
+    var d = document.getElementById("book3");
+    var e = document.getElementById("book4");
+    var f = document.getElementById("book2");
+    var screenWidth = window.innerWidth;
+    if (z.style.width === "0px") {
+      if (screenWidth <= 900) {
+        z.style.width = "63vw";
+        b.style.display = "none";
+        c.style.display = "none";
+    } else {
+        z.style.width = "40vw";
+    }
+        x.style.width = "0px";
+        y.style.width = "0px";
+        q.style.width = "0px";
+        a.style.width = "0px";
+      } else {
+        x.style.width = "0px";
+        y.style.width = "0px";
+        z.style.width = "0px";
+        q.style.width = "0px";
+        a.style.width = "0px";
+        b.style.display = "inline-flex";
+        c.style.display = "inline-flex";
+        d.style.display = "inline-flex";
+        e.style.display = "inline-flex";
+        f.style.display = "inline-flex";
+      }
+}
+
+function myFunction4() {
+    var x = document.getElementById("book1Open");
+    var y = document.getElementById("book2Open");
+    var z = document.getElementById("book3Open");
+    var q = document.getElementById("book4Open");
+    var a = document.getElementById("book0Open");
+    var b = document.getElementById("book0");
+    var c = document.getElementById("book");
+    var d = document.getElementById("book3");
+    var e = document.getElementById("book4");
+    var f = document.getElementById("book2");
+    var screenWidth = window.innerWidth;
+    if (q.style.width === "0px") {
+      if (screenWidth <= 900) {
+        q.style.width = "63vw";
+        b.style.display = "none";
+        c.style.display = "none";
+    } else {
+        q.style.width = "40vw";
+    }
+        x.style.width = "0px";
+        y.style.width = "0px";
+        z.style.width = "0px";
+        a.style.width = "0px";
+      } else {
+        x.style.width = "0px";
+        y.style.width = "0px";
+        z.style.width = "0px";
+        q.style.width = "0px";
+        a.style.width = "0px";
+        b.style.display = "inline-flex";
+        c.style.display = "inline-flex";
+        d.style.display = "inline-flex";
+        e.style.display = "inline-flex";
+        f.style.display = "inline-flex";
+      }
+}
+
+function myFunction0() {
+    var a = document.getElementById("book0Open");
+    var x = document.getElementById("book1Open");
+    var y = document.getElementById("book2Open");
+    var z = document.getElementById("book3Open");
+    var q = document.getElementById("book4Open");
+    var d = document.getElementById("book3");
+    var e = document.getElementById("book4");
+    var f = document.getElementById("book2");
+    var screenWidth = window.innerWidth;
+    if (a.style.width === "0px") {
+      if (screenWidth <= 900) {
+        a.style.width = "55vw";
+        d.style.display = "none";
+        e.style.display = "none";
+        f.style.display = "none";
+    } else {
+        a.style.width = "40vw";
+    }
+        x.style.width = "0px";
+        q.style.width = "0px";
+        y.style.width = "0px";
+        z.style.width = "0px";
+      } else {
+        a.style.width = "0px";
+        x.style.width = "0px";
+        y.style.width = "0px";
+        z.style.width = "0px";
+        q.style.width = "0px";
+        d.style.display = "inline-flex";
+        e.style.display = "inline-flex";
+        f.style.display = "inline-flex";
+      }
+}
